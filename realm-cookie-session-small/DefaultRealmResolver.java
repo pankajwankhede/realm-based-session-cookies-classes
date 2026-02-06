feae2c48be5fc603ba8c5bd9b4179d0a81808b01
Pankaj wankhede <pankajwankhede91@gmail.com>
2026-02-06 06:04:40 +0530

    Update DefaultRealmResolver.java

--- realm-cookie-session-small/DefaultRealmResolver.java
+++ realm-cookie-session-small/DefaultRealmResolver.java
@@ -9,12 +9,20 @@ public class DefaultRealmResolver implements RealmResolver {
 
     @Override
     public String resolveRealm(HttpServletRequest request) {
+
+        // 1) request parameter
         String realm = request.getParameter("realm");
-        if (StringUtils.hasText(realm)) return realm.toUpperCase();
+        if (StringUtils.hasText(realm)) {
+            return realm.trim().toUpperCase();
+        }
 
+        // 2) header (optional)
         realm = request.getHeader("X-REALM");
-        if (StringUtils.hasText(realm)) return realm.toUpperCase();
+        if (StringUtils.hasText(realm)) {
+            return realm.trim().toUpperCase();
+        }
 
+        // 3) fallback
         return "DEFAULT";
     }
 }
